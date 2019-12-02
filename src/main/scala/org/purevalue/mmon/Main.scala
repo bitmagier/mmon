f046a82ec53127d3cee8e94668f314d230ea7946
Roman Krüger <bitmagier@mailbox.org>
2019-12-02 07:30:48 +0100

    . (#2)

--- src/main/scala/org/purevalue/mmon/Main.scala
+++ src/main/scala/org/purevalue/mmon/Main.scala
@@ -11,15 +11,15 @@ object Main {
 
     if (args.isEmpty)
       log.warn("Please provide action via commandline parameter.\n"
-        + "l = InitialLoad (pure)\n"
-        + "c = Continue mode using existing database and local cached stock data\n"
+        + "l = InitialLoad\n"
+        + "c = Continue mode using existing database\n"
         + "i = Apply indicators only - requires previously loaded data"
       )
     else {
       args.head match {
-        case "l" => InitialLoad.initialLoad()
-        case "c" => InitialLoad.initialLoad(true, false)
-        case "i" => InitialLoad.applyIndicators()
+        case "l" => Load.initialLoad()
+        case "c" => Load.initialLoad(dropDatabase = false)
+        case "i" => Load.applyIndicators()
         case _ => log.error(s"Unsupported action ${args.head}")
       }
     }
